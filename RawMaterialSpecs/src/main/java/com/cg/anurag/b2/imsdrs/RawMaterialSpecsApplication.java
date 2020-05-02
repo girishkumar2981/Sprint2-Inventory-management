1ec9bc5f956c31a03b0497a1033551f00a92b3f8
girish kumar <girishkumar2981@gmail.com>
2020-05-02 09:19:35 +0530

    first commit

---
+++ RawMaterialSpecs/src/main/java/com/cg/anurag/b2/imsdrs/RawMaterialSpecsApplication.java
@@ -0,0 +1,13 @@
+package com.cg.anurag.b2.imsdrs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RawMaterialSpecsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RawMaterialSpecsApplication.class, args);
+	}
+
+}
