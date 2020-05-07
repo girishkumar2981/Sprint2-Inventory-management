830a394061769ef0da7d55ee09aef559ac3c1ffd
girish kumar <girishkumar2981@gmail.com>
2020-05-07 18:34:02 +0530

    first commit

---
+++ DisplaySupplierDetails/src/main/java/com/cg/anurag/b2/imsds/DisplaySupplierDetailsApplication.java
@@ -0,0 +1,13 @@
+package com.cg.anurag.b2.imsds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DisplaySupplierDetailsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DisplaySupplierDetailsApplication.class, args);
+	}
+
+}
